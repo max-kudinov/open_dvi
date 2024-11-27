aa7afb646939325d10d42e9495bb9675cfd1c55c
Maxim Kudinov <max.kudinov@proton.me>
2024-11-27 17:36:57 +0300

    Minor refactor

--- board_top.sv
+++ board_top.sv
@@ -1,6 +1,7 @@
 module board_top (
     input  logic       clk_i,
     input  logic       rst_n_i,
+    output logic       led,
     output logic       tmds_clk_n,
     output logic       tmds_clk_p,
     output logic [2:0] tmds_data_n,
@@ -9,47 +10,62 @@ module board_top (
 
     logic rst;
     logic serial_clk;
-    logic pixel_clk_raw;
+    logic pixel_clk_div2;
     logic pixel_clk;
     logic pll_lock;
 
+    logic [23:0] cnt;
+
     assign rst = ~rst_n_i;
 
-    CLKDIV #(
-        .DIV_MODE ("5")
-    ) div_5 (
-        .HCLKIN (serial_clk),
-        .CLKOUT (pixel_clk_raw),
-        .RESETN (pll_lock)
-    );
+    always_ff @(posedge pixel_clk) begin
+        cnt <= cnt + 1'b1;
+    end
 
-    CLKDIV #(
-        .DIV_MODE ("2")
-    ) div_2 (
-        .HCLKIN (pixel_clk_raw),
-        .CLKOUT (pixel_clk),
-        .RESETN (pll_lock)
-    );
+    assign led = cnt[23];
 
-    rPLL #(
-        .FCLKIN    ( "27" ),
-        .IDIV_SEL  ( 2    ), // -> PFD = 9 MHz (range: 3-400 MHz)
-        .FBDIV_SEL ( 27   ), // -> CLKOUT = 252 MHz (range: 3.125-600 MHz)
-        .ODIV_SEL  ( 4    )  // -> VCO = 1008 MHz (range: 400-1200 MHz)
-    ) pll (
-        .RESET   ( 1'b0       ),
-        .RESET_P ( 1'b0       ),
-        .CLKFB   ( 1'b0       ),
-        .FBDSEL  ( 6'b0       ),
-        .IDSEL   ( 6'b0       ),
-        .ODSEL   ( 6'b0       ),
-        .PSDA    ( 4'b0       ),
-        .DUTYDA  ( 4'b0       ),
-        .FDLY    ( 4'b0       ),
-        .CLKIN   ( clk_i      ), // 27 MHz
-        .CLKOUT  ( serial_clk ),  // 252 MHz
-        .LOCK    ( pll_lock   )
-    );
+
+    `ifndef VERILATOR
+
+        // Create 252 MHz clock for serializer
+
+        rPLL #(
+            .FCLKIN    ( "27" ),
+            .IDIV_SEL  ( 2    ),
+            .FBDIV_SEL ( 27   ),
+            .ODIV_SEL  ( 4    )
+        ) rpll_inst (
+            .CLKIN   ( clk_i      ), // 27 MHZ
+            .CLKOUT  ( serial_clk ), // 252 MHz
+            .LOCK    ( pll_lock   ),
+            .RESET   ( '0         ),
+            .RESET_P ( '0         ),
+            .CLKFB   ( '0         ),
+            .FBDSEL  ( '0         ),
+            .IDSEL   ( '0         ),
+            .ODSEL   ( '0         ),
+            .PSDA    ( '0         ),
+            .DUTYDA  ( '0         ),
+            .FDLY    ( '0         )
+        );
+
+        // Divide by 10 to get 25.2 MHz pixel clock
+
+        CLKDIV2 div_2 (
+            .HCLKIN ( serial_clk     ),
+            .CLKOUT ( pixel_clk_div2 ),
+            .RESETN ( pll_lock       )
+        );
+
+        CLKDIV #(
+            .DIV_MODE ("5")
+        ) div_5 (
+            .HCLKIN ( pixel_clk_div2 ),
+            .CLKOUT ( pixel_clk      ),
+            .RESETN ( pll_lock       )
+        );
+
+    `endif
 
     dvi_top i_dvi_top (
         .serial_clk_i ( serial_clk  ),
